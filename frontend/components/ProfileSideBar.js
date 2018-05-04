5aa892713cd95f814a6fbf94f06c3282d24cd3a3
Ana Gheorghe <a.s.gheorghe3@gmail.com>
2018-05-04 14:47:12 -0700

    profile waiting for picture url to be fixed

--- frontend/components/ProfileSideBar.js
+++ frontend/components/ProfileSideBar.js
@@ -15,13 +15,13 @@ render() {
   return (
     <div>
       <Dropzone onDrop={this.props.onDropPicture}>
-        {this.props.pictureUrl === "" ?
+        {this.props.basic.picture_url === "" ?
         <div> Upload a picture </div> :
-        <img style={{"height": "100%"}} src={this.props.basic.pictureUrl} />}
+        <img style={{"height": "100%"}} src={this.props.basic.picture_url} />}
       </Dropzone>
       <div> <b>{this.props.basic.first_name} {this.props.basic.last_name}</b> </div>
       <div> {this.props.basic.location}</div>
-      <div> {this.props.eduArr[0].major} @ {this.props.eduArr[0].school}</div>
+      <div> {this.props.eduArr[0].major} @ {this.props.eduArr[0].school_name}</div>
       <Link style={{border: '1px solid black', width: '150px', margin: '5px'}} to="/cand/selfprofile#work">Work Experience</Link>
       <Link style={{border: '1px solid black', width: '150px', margin: '5px'}} to="/cand/selfprofile#projects">Projects</Link>
       <Link style={{border: '1px solid black', width: '150px', margin: '5px'}} to="/cand/selfprofile#skills">Skills</Link>
